4fc1a2c2d9e81f8992bb3eb28d05799782be1487
haryobagus <haryobagus14@gmail.com>
2019-11-02 17:05:51 -0600

    percobaan 2

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -2,6 +2,7 @@ package id.ac.polinema.colorchangernormal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -12,30 +13,34 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-	private ConstraintLayout rootView;
-	private Button btnChangeColor;
+    ConstraintLayout rootView;
+    Button btnChangeColor;
+    ColorViewModel colorViewModel;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		rootView = findViewById(R.id.layout_main);
-		btnChangeColor = findViewById(R.id.change_color);
-		
-		rootView.setBackgroundColor(generateRandomColor());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rootView = findViewById(R.id.layout_main);
+        btnChangeColor = findViewById(R.id.change_color);
+        colorViewModel = ViewModelProviders.of(this).get(ColorViewModel.class);
 
-		btnChangeColor.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				int color = generateRandomColor();
-				rootView.setBackgroundColor(color);
-			}
-		});
-	}
+//		rootView.setBackgroundColor(generateRandomColor());
+        rootView.setBackgroundColor(colorViewModel.getColor());
 
-	private int generateRandomColor(){
-		Random rnd = new Random();
-		return Color.argb(255,rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256));
-	}
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int color = generateRandomColor();
+                rootView.setBackgroundColor(color);
+                colorViewModel.setColor(color);
+            }
+        });
+    }
+
+    private int generateRandomColor(){
+        Random rnd = new Random();
+        return Color.argb(255,rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256));
+    }
 
 }
